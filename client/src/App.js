2048ca3fce5e5be6d8553b8a76c6683829dd5046
Nick Lewis <nick@TechEd-0006.techeducators.co.uk>
2023-07-27 16:49:28 +0100

    wickywa1

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,28 @@ import axios from "axios";
 
 function App() {
   const [outcome, setOutcome] = useState("");
+  const [dummy, setDummy] = useState(0);
+
+  useEffect(() => {
+    console.log("useEffect has been triggered");
+    handleSurvive();
+  }, [dummy]);
 
   async function handleSurvive(event) {
-    const API = "http://localhost:8080/survive";
+    const API = "https://wickywickywa.onrender.com/survive";
     const res = await axios.get(API);
-    console.log(res);
+    console.log(res.status);
     setOutcome(res.data);
+
+    const firstWord = res.data.replace(/ .*/, "");
+    console.log(firstWord);
   }
-  return (
-    <div className="App">
-      <h1>placeholder</h1>
-      <button onClick={handleSurvive}>Brave the Oregon Trail!</button>
-    </div>
-  );
+
+  function insultJez() {
+    setDummy(dummy + 1);
+  }
+
+  return <div className="App">{outcome}</div>;
 }
 
 export default App;
